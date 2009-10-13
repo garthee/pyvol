942544af36433bb61b85ba5d0a8120837a2fe0e1
emin.martinian <emin.martinian@3e17aeee-b7f2-11de-b129-73e2b2a9d7e9>
2009-10-13 15:24:22 +0000

    (EM)	Added more docs and doctests.

--- pyvol/experimental/ExampleEstimators.py
+++ pyvol/experimental/ExampleEstimators.py
@@ -1,4 +1,64 @@
 """Module containing example estimators illustrating how to write your own.
+
+    If you want to add your own estimators, you can create a file called
+    MyVolEst.py and either put it outside the pyvol source tree or put it
+    into pyvol/experimental if you are planning on submitting your
+    estimator in a GenericContest.  
+
+    Once you have created your own estimator, you can test it by starting
+    the python interpreter and doing something like the following: 
+
+>>> import random, datetime
+>>> from pyvol.experimental import ExampleEstimators # or import your stuff
+>>> from pyvol.sims import Evaluator  # used to evaluate performance
+>>> estimtator = ExampleEstimators.ExponentialCovEstimator(
+... .99) # try simple exp smoothing or replaec with our own estimator
+>>> results = Evaluator.EvalEstimator(estimtator, seed=64) # do simulation
+>>> print results[0]
+yearly    : ret = 0.026, vol = 0.195, sqCorrs = 1.000, 0.123, -0.084
+quarterly : ret = 0.025, vol = 0.222, sqCorrs = 1.000, 0.103, 0.138
+monthly   : ret = 0.026, vol = 0.223, sqCorrs = 1.000, 0.054, 0.158
+weekly    : ret = 0.026, vol = 0.242, sqCorrs = 1.000, 0.175, 0.249
+daily     : ret = 0.026, vol = 0.250, sqCorrs = 1.000, 0.422, 0.276
+targErr= 0.02478, spreadErr= 144.79175, naiveError= 7.86425, minPossible=2.05384
+
+
+    If you want to do more detailed work, you can walk through the
+    steps of generating the simulation yourself as shown below:
+
+>>> import random; random.seed(64) # make things repeatable    
+>>> from pyvol.sims import DataSimulator, SimAPI
+>>> from pyvol.est import CovEst
+>>> simulator = DataSimulator.PriceSimulator(
+... startDate=datetime.date(2000, 1, 3), endDate=datetime.date(2009, 1, 5))
+>>> returnColName = simulator.levelParams.retNames[-1]
+>>> estimator = ExampleEstimators.FullHistoryCovEstimator()
+>>> estimator.SetReturnColName(returnColName)
+>>> estimatorTx = CovEst.EstimateVols(estimator, 'estimatedVol')
+>>> query = simulator.MakeSimulatedData([estimatorTx])
+>>> stats = SimAPI.MakeStats(query, simulator.levelParams)
+>>> print stats
+yearly    : ret = 0.129, vol = 0.286, sqCorrs = 1.000, -0.316, -0.022
+quarterly : ret = 0.132, vol = 0.235, sqCorrs = 1.000, -0.123, 0.091
+monthly   : ret = 0.142, vol = 0.216, sqCorrs = 1.000, -0.048, -0.023
+weekly    : ret = 0.141, vol = 0.262, sqCorrs = 1.000, 0.136, 0.308
+daily     : ret = 0.144, vol = 0.272, sqCorrs = 1.000, 0.439, 0.355
+
+>>> import logging;
+>>> logging.info('''
+...          If you have widnows and the windows python tools installed
+...          you can do the following to display the data in Excel:
+...
+... from pyvol.tseries import ExcelTools; ExcelTools.TimeSeqToExcel(results[1])
+...
+...          If not, you can just write the output to a file to investigate
+...          or look directly at results[1].data.
+... ''')
+>>> import tempfile, os
+>>> filename = tempfile.mktemp(suffix='_pyvol_sim.csv')
+>>> results[1].WriteToSimpleCSVFile(filename)
+>>> os.remove(filename) # remove the file when you are don with it.
+    
 """
 
 import math
@@ -129,3 +189,13 @@ targErr= 0.01283, spreadErr= 53.26522, naiveError= 7.92135, minPossible=2.06377
                           + self.smoothing * squaredReturn)
             
         return math.sqrt(self.state) if self.state > 0 else None
+
+
+def _test():
+    "Test docstrings in module."
+    import doctest
+    doctest.testmod()
+
+if __name__ == "__main__":    
+    _test()
+    print 'Test finished.'
